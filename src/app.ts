aa977dbaddfd6f543cd990d22cc63a6665582f64
Amr Mohamed <amrmuhmedd@gmail.com>
2024-12-12 00:42:03 +0200

    fix: fix swagger ui in vercel

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,12 @@ class App {
     this.app.use(
       '/api-docs',
       swaggerUi.serve as any,
-      swaggerUi.setup(swaggerConfig) as any
+      swaggerUi.setup(swaggerConfig , {
+        customCss:
+        ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+      customCssUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+      }) as any
     );
   }
 
